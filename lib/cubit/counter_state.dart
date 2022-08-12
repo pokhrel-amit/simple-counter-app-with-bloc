febfc09aab6ad92dc43a33e675e2acb12cebdb2a
amit pokhrel <contact.amitpok@gmail.com>
2022-08-12 22:05:43 +0545

    completed

--- lib/cubit/counter_state.dart
+++ lib/cubit/counter_state.dart
@@ -1,7 +1,13 @@
 import 'dart:developer';
 
-class CounterState {
+import 'package:equatable/equatable.dart';
+
+class CounterState extends Equatable {
   final counterValue;
   final wasIncremented;
   CounterState({ required this.counterValue,this.wasIncremented});
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [this.counterValue,this.wasIncremented];
 }
