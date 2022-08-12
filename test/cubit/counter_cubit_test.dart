e4d9ec3a3f1711b3ca9158524ac294a217fa7a2d
amit pokhrel <contact.amitpok@gmail.com>
2022-08-12 21:58:50 +0545

    feat:test cases added

---
+++ test/cubit/counter_cubit_test.dart
@@ -0,0 +1,30 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sm_bloc_cubit/cubit/counter_cubit.dart';
+import 'package:sm_bloc_cubit/cubit/counter_state.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group(CounterCubit, (() {
+    CounterCubit? counterCubit;
+    setUp((() {
+      counterCubit = CounterCubit();
+    }));
+
+    tearDown((() {
+      counterCubit?.close();
+    }));
+
+    test('the initial state of CounterCubit is CounterState(counterValue:0)',
+        (() {
+      expect(counterCubit?.state, CounterState(counterValue: 0));
+    }));
+    blocTest(
+      'the cubit should emit CounterState(counterValue:1,wasIncrement:false) when cubit.increment function is called',
+      build: () => CounterCubit(),
+      act: (CounterCubit? cubit) => cubit?.increment(),
+      expect: () =>
+          <CounterState>[CounterState(counterValue: 1, wasIncremented: true)],
+    );
+  }));
+}
